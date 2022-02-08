0bd7978770b29d1ee7ab792e4133b47cb26cdb1c
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-08 04:45:25 +0300

    Kalan saniye bulma

---
+++ calısma.java
@@ -0,0 +1,13 @@
+package Operatorler;
+
+public class calısma {
+
+    public static void main(String[] args) {
+
+        int saniye = 5489;
+        int dakika = saniye / 60;
+        int artansaniye = saniye % 60;
+
+        System.out.println("DAKİKA : " + dakika + "  --  SANİYE : " + saniye + "  --  ARTAN SANİYE : " + artansaniye);
+    }
+}
