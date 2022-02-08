5fda27316dd2583ea1ac33ec609a6a06ec8c3a30
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-08 14:08:24 +0300

    Fahrenheit

---
+++ calısma1.java
@@ -0,0 +1,12 @@
+package Operatorler;
+
+public class calısma1 {
+
+    public static void main(String[] args) {
+
+        double fahrenheit = 100.0;
+        double celcius = (5.0/9.0) * (fahrenheit - 32);
+
+        System.out.println(fahrenheit + "  Fahrenheit = " + celcius + "  Celcius");
+    }
+}
