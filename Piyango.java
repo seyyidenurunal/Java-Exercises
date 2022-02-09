2693d8d1d60091845a07b3601248ac3a0cfa1c5a
seyyidenurunal <95251609+seyyidenurunal@users.noreply.github.com>
2022-02-09 23:04:20 +0300

    Piyango

--- Piyango.java
+++ Piyango.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Piyango {
     public static void main(String[] args) {
+        
 
         System.out.println("JAVA ÇEKİLİŞİNE HOŞ GELDİNİZ...");
 
@@ -12,10 +13,21 @@ public class Piyango {
         Scanner tara = new Scanner(System.in);
         System.out.println("Lütfen Tahmininizi Giriniz...");
         int tahminEdilenSayi = tara.nextInt();
+        
+        int sansliSayiBirinciBasamak = sansliSayi/10;
+        int sansliSayiIkinciBasamak = sansliSayi %10;
+
+        int tahminEdilenSayiBirinciBasamak = tahminEdilenSayi/10;
+        int tahminEdilenSayiIkinciBasamak = tahminEdilenSayi %10;
 
         if (tahminEdilenSayi == sansliSayi){
-            System.out.println("TEBRİKLER 1000 TL KAZANDINIZ !\nŞanslı sayı : " +sansliSayi);
+            System.out.println("TEBRİKLER 10000 TL KAZANDINIZ !\nŞanslı sayı : " +sansliSayi);
 
+        }else if (sansliSayiBirinciBasamak == tahminEdilenSayiIkinciBasamak && sansliSayiIkinciBasamak == tahminEdilenSayiBirinciBasamak) {
+            System.out.println("TEBRİKLER! 5000 TL KAZANDINIZ.\n Şanslı Sayı : " + sansliSayi);
+
+        }else if(sansliSayiBirinciBasamak == tahminEdilenSayiBirinciBasamak || sansliSayiIkinciBasamak == tahminEdilenSayiIkinciBasamak || sansliSayiBirinciBasamak == tahminEdilenSayiIkinciBasamak || sansliSayiIkinciBasamak == tahminEdilenSayiBirinciBasamak) {
+            System.out.println("TEBRİKLER! 2500 TL KAZANDINIZ.\n Şanslı Sayı : " + sansliSayi);
         }else {
                 System.out.println("Lütfen Tekrar Deneyiniz...\n Şanslı sayı : " + sansliSayi);
 
